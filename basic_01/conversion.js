6500b7ee452e6d3cb8a0af12a30f0feb1d36de79
Pranshu <pranshupatel1605@gmail.com>
2025-07-27 11:25:57 +0530

    conversion

---
+++ basic_01/conversion.js
@@ -0,0 +1,37 @@
+// string to number 
+let numstring="23"
+console.log(numstring)
+let valueinnum= Number(numstring)
+console.log(typeof valueinnum)
+console.log(valueinnum)
+// null to number
+let nullvar= null
+console.log(typeof nullvar)
+console.log(nullvar)
+valueinnum= Number(nullvar)
+console.log(typeof valueinnum)
+console.log(valueinnum)
+// undefined to number
+let undefinedvar=undefined
+console.log(typeof undefinedvar)
+console.log(undefinedvar)
+valueinnum= Number(undefinedvar)
+console.log(typeof valueinnum)
+console.log(valueinnum)
+// string to number 
+let string="Pranshu"
+console.log(string)
+valueinnum= Number(string)
+console.log(typeof valueinnum)
+console.log(valueinnum)
+// boolean to number
+let boolvar=false
+console.log(typeof boolvar)
+console.log(boolvar)
+valueinnum= Number(boolvar)
+console.log(typeof valueinnum)
+console.log(valueinnum)
+// num value including + opt. converts num to string
+console.log("37"+7)
+console.log("37"-7)
+console.log("37"*7)
