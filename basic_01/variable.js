bd636d70449830ee6f6e9f63718744ac1d43d7a5
Pranshu <pranshupatel1605@gmail.com>
2025-07-27 16:06:50 +0530

    variable

---
+++ basic_01/variable.js
@@ -0,0 +1,19 @@
+const accountid= 45
+
+let accountemail="sds65@gmail.com"
+
+var accountpassword="555"
+
+accountname="sds"
+
+// accountid= 222
+accountemail="abc55@gmail.com"
+accountpassword="5545"
+accountname="abc"
+
+console.log([
+    accountid,
+    accountemail,
+    accountpassword,
+    accountname,
+])
