b584db0742a2d73daa137771eacd494a80d22122
Pranshu <pranshupatel1605@gmail.com>
2025-07-27 17:32:19 +0530

    conversion

--- basic_01/conversion.js
+++ basic_01/conversion.js
@@ -4,6 +4,7 @@ console.log(numstring)
 let valueinnum= Number(numstring)
 console.log(typeof valueinnum)
 console.log(valueinnum)
+
 // null to number
 let nullvar= null
 console.log(typeof nullvar)
@@ -11,6 +12,7 @@ console.log(nullvar)
 valueinnum= Number(nullvar)
 console.log(typeof valueinnum)
 console.log(valueinnum)
+
 // undefined to number
 let undefinedvar=undefined
 console.log(typeof undefinedvar)
@@ -18,12 +20,14 @@ console.log(undefinedvar)
 valueinnum= Number(undefinedvar)
 console.log(typeof valueinnum)
 console.log(valueinnum)
+
 // string to number 
 let string="Pranshu"
 console.log(string)
 valueinnum= Number(string)
 console.log(typeof valueinnum)
 console.log(valueinnum)
+
 // boolean to number
 let boolvar=false
 console.log(typeof boolvar)
@@ -31,6 +35,7 @@ console.log(boolvar)
 valueinnum= Number(boolvar)
 console.log(typeof valueinnum)
 console.log(valueinnum)
+
 // num value including + opt. converts num to string
 console.log("37"+7)
 console.log("37"-7)
